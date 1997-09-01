6c03e6004c4c177bc29ad33da229b63cb8dbe3ab
IbrarSakhi <115551824+IbrarSakhi@users.noreply.github.com>
1997-09-01 20:34:55 +0500

    Update psexec-script.bat

--- TestProject/psexec-script.bat
+++ TestProject/psexec-script.bat
@@ -2,5 +2,5 @@
 setlocal
 
 REM Use PsExec to launch application as an admin
-PsExec.exe -i 1 -h -d -u POWER\ibrar.sakhi -p New14266@ "C:\Users\ibrar.sakhi\Desktop\RunTestStandFromJenkin\ConsoleApp1\bin\Debug\ConsoleApp1.exe"
+PsExec.exe -i 1 -h -d -u POWER\ibrar.sakhi -p New14266@ "J:\SW-E2E\01-ATM Repository\Code Workspace\Working\RunTestStandsSeq.exe"
 endlocal
