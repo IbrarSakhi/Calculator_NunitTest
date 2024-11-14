c8fed62d96288e8f8ba740c12a0f193b6b5e4462
IbrarSakhi <115551824+IbrarSakhi@users.noreply.github.com>
2024-11-14 17:32:27 +0500

    Update LaunchApp.bat

--- TestProject/LaunchApp.bat
+++ TestProject/LaunchApp.bat
@@ -1,2 +1,16 @@
 @echo off
+setlocal
+
+REM Check if running as Administrator
+openfiles >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Restarting script with Administrator privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+REM If the script is running as Admin, execute your command here
+echo Running command as administrator...
 start "" "C:\Program Files (x86)\Smart Wires\SmartInterface\SmartInterface.exe"
+
+endlocal
