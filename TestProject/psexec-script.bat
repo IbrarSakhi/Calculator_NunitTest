92a3c417eafe9be2100123ac54ad5f2ec1f5032d
IbrarSakhi <115551824+IbrarSakhi@users.noreply.github.com>
2024-12-11 16:19:58 +0500

    Update psexec-script.bat

--- TestProject/psexec-script.bat
+++ TestProject/psexec-script.bat
@@ -2,6 +2,6 @@
 setlocal
 
 REM Use PsExec to launch application as an admin
-PsExec.exe -i 1 -h -d -u POWER\ibrar.sakhi -p Zaroon7890@ "C:\Program Files (x86)\Smart Wires\SmartInterface\SmartInterface.exe"
+PsExec.exe -i 1 -h -d -u POWER\ibrar.sakhi -p Zaroon7890@ "C:\Program Files\National Instruments\TestStand 2016\UserInterfaces\Full-Featured\CSharp\Source Code\Bin\x64\release\TestExec.exe" -run MainSequence "J:\SW-E2E\01-ATM Repository\Scripts Workspace\Scripts\01 Core APIs Scripts\OpenSmartInterface.seq"
 
 endlocal
