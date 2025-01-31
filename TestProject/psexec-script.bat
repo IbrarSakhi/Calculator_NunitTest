0874702cefc36b24bee203e8979b4fa514e391a5
IbrarSakhi <115551824+IbrarSakhi@users.noreply.github.com>
2025-01-31 19:21:19 +0500

    Update

--- TestProject/psexec-script.bat
+++ TestProject/psexec-script.bat
@@ -2,5 +2,5 @@
 setlocal
 
 REM Use PsExec to launch application as an admin
-PsExec.exe -i 1 -h -d -u POWER\ibrar.sakhi -p New14266@ "J:\SW-E2E\01-ATM Repository\Code Workspace\Working\RunTestStandsSeq.exe"
+PsExec.exe -i 1 -h -d -u POWER\E2EATM-DSKTP1 -p sw@atm "J:\SW-E2E\01-ATM Repository\Code Workspace\Working\RunTestStandsSeq.exe"
 endlocal
