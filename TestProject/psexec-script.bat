3802f23c09e97a0e583b6916839c37ba19d32370
Smart Wires ATM <smartwiresatm@Powersoft19.com>
2025-04-10 17:21:57 +0500

    Update psexec-script.bat

--- TestProject/psexec-script.bat
+++ TestProject/psexec-script.bat
@@ -2,5 +2,6 @@
 setlocal
 
 REM Use PsExec to launch application as an admin
-PsExec.exe -i 1 -s -h -d -u POWER\smartwiresatm -p sw@atm "C:\Users\smartwiresatm\Desktop\OTA\OTA\bin\Debug\OTA.exe"
+PsExec.exe -i 1 -h -d -u POWER\smartwiresatm -p sw@atm "C:\Users\smartwiresatm\Desktop\OTA\OTA\bin\Debug\OTA.exe"
+
 endlocal
