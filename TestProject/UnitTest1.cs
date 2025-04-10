34391b2242bd0236ec24b6346565f714e4522fd0
Smart Wires ATM <smartwiresatm@Powersoft19.com>
2025-04-10 20:26:26 +0500

    Update UnitTest1.cs

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -162,7 +162,6 @@ namespace TestProject
             }
         }
         [Test, Order(1)]
-        [Ignore("Ignore a test")]
         public void RunBatScriptWithPsExec()
         {
             //DateTime updatedDateTime = DateTime.Now.AddDays(-10000);
