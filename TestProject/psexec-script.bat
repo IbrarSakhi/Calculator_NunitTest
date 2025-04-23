e37416536415ec21b00daf7b439671311909d7f4
IbrarSakhi <115551824+IbrarSakhi@users.noreply.github.com>
2025-04-23 15:51:43 +0500

    Update psexec-script.bat

--- TestProject/psexec-script.bat
+++ TestProject/psexec-script.bat
@@ -2,5 +2,5 @@
 setlocal
 
 REM Use PsExec to launch application as an admin
-PsExec.exe -i 1 -s -h -d -u POWER\ibrar.sakhi -p New14266@ "C:\Users\ibrar.sakhi\Desktop\OTA\OTA\bin\Debug\OTA.exe"
+PsExec.exe -i 1 -s -h -d -u POWER\ibrar.sakhi -p AllahBadshah1# "C:\Users\ibrar.sakhi\Desktop\OTA\OTA\bin\Debug\OTA.exe"
 endlocal
