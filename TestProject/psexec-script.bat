b4e576c3b099042408aa2944d38dc3ce3dd031a8
IbrarSakhi <115551824+IbrarSakhi@users.noreply.github.com>
2025-10-07 20:03:07 +0500

    Update psexec-script.bat

--- TestProject/psexec-script.bat
+++ TestProject/psexec-script.bat
@@ -2,6 +2,6 @@
 setlocal
 
 REM Use PsExec to launch application as an admin
-PsExec.exe -i  1 -h -d -u POWER\smartwiresatm -p sw@atm "C:\Users\smartwiresatm\Desktop\SWIFT\Code Workspace\Working\SWIFTATMTestApplication.exe"
+PsExec.exe -i  1 -h -d -u POWER\smartwiresatm -p sw@atm "C:\Users\smartwiresatm\Desktop\OTA\OTA\OTA\OTA\bin\Debug\OTA.exe"
 
 endlocal
